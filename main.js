ac6c2ebfaa7f0b30b77e1757e8d1c113e2f3f34b
Aiganysh26 <aiganyshbeisheeva7@gmail.com>
2023-06-04 15:33:26 +0200

    added random img

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const getData = async () => {
   try {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts')
   const data = await response.json()
-  data.forEach(card => {
+  data.slice(1, 50).forEach(card => {
     let cardElement = document.createElement('div')
     cardElement.setAttribute('class', 'news_card')
     cardElement.innerHTML = `
-      <img src="./image/news.jpg" alt="nature">
+      <img src="https://picsum.photos/400/200?random=${card.id}" alt="nature">
       <h2>${card.title}</h2>
       <p>${card.body}</p> `
     cards.append(cardElement)
